8b08621666151a12efe1d771677906acfe32d7f4
Anshu Aggarwal <aaggarwal2805@gmail.com>
2019-07-01 15:07:07 -0400

    Hide password

--- src/sql.py
+++ src/sql.py
@@ -1,7 +1,7 @@
 DB_HOST ="sql-instance.cjxcefmwyzy2.us-east-1.rds.amazonaws.com"
 DB_PORT = 3306
 DB_USER ="dbuser"
-DB_PASSWD ="Iphone6AA"
+DB_PASSWD ="****"
 DB_NAME ="parkingdb"
 import MySQLdb
 mydb = MySQLdb.connect(host = DB_HOST,port = DB_PORT,user = DB_USER,passwd= DB_PASSWD,db= DB_NAME)
