2723d7bf9b719cd911c5cc2aaae05b83c83d5be3
Anshu Aggarwal <aaggarwal2805@gmail.com>
2019-07-06 17:39:08 -0400

    changes in code

--- src/sql.py
+++ src/sql.py
@@ -1,10 +1,10 @@
-DB_HOST ="sql-instance.cjxcefmwyzy2.us-east-1.rds.amazonaws.com"
-DB_PORT = 3306
+DB_HOST ="rds-instance.cjxcefmwyzy2.us-east-1.rds.amazonaws.com"
+DB_PORT = 5432
 DB_USER ="dbuser"
 DB_PASSWD ="****"
 DB_NAME ="parkingdb"
-import MySQLdb
-mydb = MySQLdb.connect(host = DB_HOST,port = DB_PORT,user = DB_USER,passwd= DB_PASSWD,db= DB_NAME)
+import psycopg
+mydb = psycopg.connect(host = DB_HOST,port = DB_PORT,user = DB_USER,passwd= DB_PASSWD,db= DB_NAME)
 mycursor = mydb.cursor()
 sql="show schemas"
 mycursor.execute(sql)
